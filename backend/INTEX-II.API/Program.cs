e3309a34254848251a9a88ab05f782db3a5598c8
Tasedit <tasedit@gmail.com>
2025-04-10 13:32:13 -0600

    hopefully fixed

--- backend/INTEX-II.API/Program.cs
+++ backend/INTEX-II.API/Program.cs
@@ -58,7 +58,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173", "https://red-smoke-085655e1e.6.azurestaticapps.net")
+            policy.WithOrigins("http://localhost:5173", "https://gray-desert-04c503d1e.6.azurestaticapps.net/", "https://red-smoke-085655e1e.6.azurestaticapps.net")
                 .AllowCredentials()
                 .AllowAnyMethod()
                 .AllowAnyHeader()
